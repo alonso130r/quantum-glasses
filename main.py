aa09c2432017125137d4b97f27d652585188259b
alonso130r <131227801+alonso130r@users.noreply.github.com>
2024-04-18 23:52:16 -0400

    finished

--- main.py
+++ main.py
@@ -17,13 +17,16 @@ special_buttons = '#bc3454'
 button_font = ('Arial', 18)
 display_font = ('Arial', 32)
 
+
 def initialize():
     global circuit
     circuit = QuantumCircuit(1)
 
+
 initialize()
 
 theta = 0
+
 
 class Glasses:
     def __init__(self):
@@ -91,37 +94,37 @@ class Glasses:
         get_input = tkinter.Tk()
         get_input.title('Get Theta')
         get_input.geometry('360x160')
-        get_input.resizable(0, 0)
+        get_input.resizable(True, True)
         val1 = tkinter.Button(get_input, height=2, width=10, bg=buttons, font=("Arial", 10), text='π/4',
-                              command=lambda: self.change_theta(0.25, get_input, circuit, key))
+                              command=lambda: self.changeTheta(0.25, get_input, circuit, key))
         val1.grid(row=0, column=0)
 
         val2 = tkinter.Button(get_input, height=2, width=10, bg=buttons, font=("Arial", 10), text='π/2',
-                              command=lambda: self.change_theta(0.50, get_input, circuit, key))
+                              command=lambda: self.changeTheta(0.50, get_input, circuit, key))
         val2.grid(row=0, column=1)
 
         val3 = tkinter.Button(get_input, height=2, width=10, bg=buttons, font=("Arial", 10), text='π',
-                              command=lambda: self.change_theta(1.0, get_input, circuit, key))
+                              command=lambda: self.changeTheta(1.0, get_input, circuit, key))
         val3.grid(row=0, column=2)
 
         val4 = tkinter.Button(get_input, height=2, width=10, bg=buttons, font=("Arial", 10), text='2π',
-                              command=lambda: self.change_theta(2.0, get_input, circuit, key))
+                              command=lambda: self.changeTheta(2.0, get_input, circuit, key))
         val4.grid(row=0, column=3, sticky='W')
 
         nval1 = tkinter.Button(get_input, height=2, width=10, bg=buttons, font=("Arial", 10), text='-π/4',
-                               command=lambda: self.change_theta(-0.25, get_input, circuit, key))
+                               command=lambda: self.changeTheta(-0.25, get_input, circuit, key))
         nval1.grid(row=1, column=0)
 
         nval2 = tkinter.Button(get_input, height=2, width=10, bg=buttons, font=("Arial", 10), text='-π/2',
-                               command=lambda: self.change_theta(-0.50, get_input, circuit, key))
+                               command=lambda: self.changeTheta(-0.50, get_input, circuit, key))
         nval2.grid(row=1, column=1)
 
         nval3 = tkinter.Button(get_input, height=2, width=10, bg=buttons, font=("Arial", 10), text='-π',
-                               command=lambda: self.change_theta(-1.0, get_input, circuit, key))
+                               command=lambda: self.changeTheta(-1.0, get_input, circuit, key))
         nval3.grid(row=1, column=2)
 
         nval4 = tkinter.Button(get_input, height=2, width=10, bg=buttons, font=("Arial", 10), text='-2π',
-                               command=lambda: self.change_theta(-2.0, get_input, circuit, key))
+                               command=lambda: self.changeTheta(-2.0, get_input, circuit, key))
         nval4.grid(row=1, column=3, sticky='W')
 
         text_object = tkinter.Text(get_input, height=20, width=20, bg="light cyan")
@@ -136,24 +139,23 @@ class Glasses:
 
         get_input.mainloop()
 
-    def main(self, testing : bool = False):
+    def main(self, testing: bool = False):
         try:
             root = tkinter.Tk()
             root.title('Quantum Glasses')
             root.geometry('399x410')
-            root.resizable(0, 0)
+            root.resizable(True, True)
             display_frame = tkinter.LabelFrame(root)
             button_frame = tkinter.LabelFrame(root, bg='black')
             display_frame.pack()
             button_frame.pack(fill='both', expand=True)
-            display = tkinter.Entry(display_frame, width=120, font=display_font, bg=background, borderwidth=2, justify=LEFT)
+            display = tkinter.Entry(display_frame, width=120, font=display_font, bg=background, borderwidth=2,
+                                    justify=tkinter.LEFT)
             display.pack(padx=3, pady=4)
 
             def display_gate(gate_input):
                 display.insert(tkinter.END, gate_input)
 
-                # Check if the number of operations has reached ten, if yes,
-                # disable all the gate buttons
                 input_gates = display.get()
                 num_gates_pressed = len(input_gates)
                 list_input_gates = list(input_gates)
@@ -179,11 +181,11 @@ class Glasses:
 
             # Define the second row of buttons
             Rx_gate = tkinter.Button(button_frame, font=button_font, bg=buttons, text='RX',
-                                     command=lambda: [display_gate('Rx'), self.user_input(circuit, 'x')])
+                                     command=lambda: [display_gate('Rx'), self.userInput(circuit, 'x')])
             Ry_gate = tkinter.Button(button_frame, font=button_font, bg=buttons, text='RY',
-                                     command=lambda: [display_gate('Ry'), self.user_input(circuit, 'y')])
+                                     command=lambda: [display_gate('Ry'), self.userInput(circuit, 'y')])
             Rz_gate = tkinter.Button(button_frame, font=button_font, bg=buttons, text='RZ',
-                                     command=lambda: [display_gate('Rz'), self.user_input(circuit, 'z')])
+                                     command=lambda: [display_gate('Rz'), self.userInput(circuit, 'z')])
             Rx_gate.grid(row=1, column=0, columnspan=1, sticky='WE', pady=1)
             Ry_gate.grid(row=1, column=1, columnspan=1, sticky='WE', pady=1)
             Rz_gate.grid(row=1, column=2, columnspan=1, sticky='WE', pady=1)
@@ -207,5 +209,48 @@ class Glasses:
             t_gate.grid(row=3, column=0, sticky='WE', pady=1)
             td_gate.grid(row=3, column=1, sticky='WE', pady=1)
 
+            def clear(circuit):
+                # clear the display
+                display.delete(0, tkinter.END)
+
+                # reset the circuit to initial state |0>
+                initialize()
+
+                # Checks if the buttons are disabled and if so, enables them
+                if x_gate['state'] == tkinter.DISABLED:
+                    gates = [x_gate, y_gate, z_gate, Rx_gate, Ry_gate, Rz_gate, s_gate, sd_gate, t_gate, td_gate,
+                             hadamard]
+                    for gate in gates:
+                        gate.config(state=tkinter.NORMAL)
+
+            # Define the Quit and Visualize buttons
+            quit = tkinter.Button(button_frame, font=button_font, bg=special_buttons, text='Quit',
+                                  command=root.destroy)
+            visualize = tkinter.Button(button_frame, font=button_font, bg=special_buttons, text='Visualize',
+                                       command=lambda: self.visualizeCircuit(circuit, root))
+            quit.grid(row=4, column=0, columnspan=2, sticky='WE', ipadx=5, pady=1)
+            visualize.grid(row=4, column=2, columnspan=1, sticky='WE', ipadx=8, pady=1)
+
+            # Define the clear button
+            clear_button = tkinter.Button(button_frame, font=button_font, bg=special_buttons, text='Clear',
+                                          command=lambda: clear(circuit))
+            clear_button.grid(row=5, column=0, columnspan=3, sticky='WE')
+
+            # Define the about button
+            about_button = tkinter.Button(button_frame, font=button_font, bg=special_buttons, text='About',
+                                          command=self.about)
+            about_button.grid(row=6, column=0, columnspan=3, sticky='WE')
+
+            if testing:
+                root.mainloop()  # Change this line
+            else:
+                root.mainloop()
+            return True
+
         except Exception as get_exception:
             raise Exception from get_exception
+
+
+if __name__ == "__main__":
+    glasses = Glasses()
+    glasses.main()
